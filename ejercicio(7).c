9f8a542e9536e299f28589a4649da5c45e126170
vivinaCordova <viviana.e.cordova@unl.edu.ec>
2024-07-08 06:53:24 -0600

    Update ejercicio(7).c

--- ejercicio(7).c
+++ ejercicio(7).c
@@ -1,10 +1,33 @@
 //cuantas filas y columnas tiene una matriz
 #include <stdio.h>
-int main (){
-    int j,i;
-    int dimencion;
-    scanf("%d \n%d %d", &dimencion, i, j);
-    scanf("%d", &dimencion);
-    printf ("ingrese una matriz");
-    dimencion[i][j];
+int main() {
+    int filas, columnas;
+    printf("Ingrese el número de filas: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas: ");
+    scanf("%d", &columnas);
+    int matriz[filas][columnas];
+    // Ingresar los valores en la matriz
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("Ingrese el valor para la posición [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    // Verificar si la matriz es cuadrada
+    if (filas == columnas) {
+        printf("¡La matriz es cuadrada!\n");
+    }
+    // Mostrar los elementos pares de la matriz
+    printf("Los elementos pares de la matriz son: \n");
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            if (matriz[i][j] % 2 == 0) {
+                printf("%d ", matriz[i][j]);
+            }
+        }
+    }
+    printf("\n");
+    
+    return 0;
 }
