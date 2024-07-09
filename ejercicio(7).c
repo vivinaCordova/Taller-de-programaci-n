cecf84fecb616ef932b057278e42a1e4ab2aaddf
vivinaCordova <viviana.e.cordova@unl.edu.ec>
2024-07-09 04:45:57 -0600

    Update ejercicio(7).c

--- ejercicio(7).c
+++ ejercicio(7).c
@@ -1,33 +1,40 @@
-//cuantas filas y columnas tiene una matriz
+//Generar la matriz simetrica
 #include <stdio.h>
+void matrizT(int matriz[3][3]){
+      for(int i=0; i<3;i++){
+       for(int j=0; j<3; j++){
+           printf(" %d ", matriz[j][i]);
+       }
+       printf("\n");
+    }
+}
 int main() {
-    int filas, columnas;
-    printf("Ingrese el número de filas: ");
-    scanf("%d", &filas);
-    printf("Ingrese el número de columnas: ");
-    scanf("%d", &columnas);
-    int matriz[filas][columnas];
-    // Ingresar los valores en la matriz
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            printf("Ingrese el valor para la posición [%d][%d]: ", i, j);
-            scanf("%d", &matriz[i][j]);
-        }
-    }
-    // Verificar si la matriz es cuadrada
-    if (filas == columnas) {
-        printf("¡La matriz es cuadrada!\n");
-    }
-    // Mostrar los elementos pares de la matriz
-    printf("Los elementos pares de la matriz son: \n");
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            if (matriz[i][j] % 2 == 0) {
-                printf("%d ", matriz[i][j]);
-            }
-        }
-    }
-    printf("\n");
-    
-    return 0;
+    printf("matriz original\n");
+   int matriz[3][3];
+   matriz[0][0]=4;
+   matriz[0][1]=1;
+   matriz[0][2]=8;
+   matriz[1][0]=1;
+   matriz[1][1]=6;
+   matriz[1][2]=7;
+   matriz[2][0]=8;
+   matriz[2][1]=7;
+   matriz[2][2]=2;
+   for(int i=0; i<3;i++){
+       for(int j=0; j<3; j++){
+           printf(" %d ", matriz[i][j]);
+       }
+       printf("\n");
+   }printf("\n");
+   printf("¿la matriz es simetrica?\n");
+   char si[40];
+   scanf("%c", &si);
+    if(si=="si"){
+        printf("correcto la matriz es simetrica\n");
+        matrizT(matriz);
+   }else{
+       printf("la matriz si es simetrica\n");
+        matrizT(matriz);
+   }
+   return 0;
 }
